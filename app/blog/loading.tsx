bcde5cb8f4e246badf3264352695c1c115eef944
Umutoni123-aa <u.nada@alustudent.com>
2026-07-08 22:49:18 +0200

    feat: add loading state for blog section

---
+++ app/blog/loading.tsx
@@ -0,0 +1,10 @@
+export default function BlogLoading() {
+  return (
+    <div className="animate-pulse space-y-4">
+      <div className="h-4 bg-gray-200 rounded w-1/3" />
+      <div className="h-24 bg-gray-200 rounded" />
+      <div className="h-24 bg-gray-200 rounded" />
+      <div className="h-24 bg-gray-200 rounded" />
+    </div>
+  );
+}
